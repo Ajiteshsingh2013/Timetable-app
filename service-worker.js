bfdf16fdeb81e6b9ef5200e92fca0e6e36e46d88
Ajiteshsingh2013 <pk.aastha1982@gmail.com>
2025-05-18 16:34:34 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+      caches.open("timetable-v1").then((cache) => {
+        return cache.addAll([
+          "/",
+          "/index.html",
+          "/style.css",
+          "/script.js",
+          "/manifest.json",
+          "/icon-192.png",
+          "/icon-512.png"
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", (e) => {
+    e.respondWith(
+      caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+  });
+  
